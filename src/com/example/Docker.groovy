2848c608749499139826b905c9e75b799b43f60c
medch1 <chachia.mohamed@gmail.com>
2022-08-13 16:18:15 +0100

    docker login func

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -11,8 +11,12 @@ class Docker implements Serializable {
 
     }
 
-    def pushDockerImage(String tag){
+    def dockerLogin(){
         script.sh "echo $script.DOCKERHUB_CREDENTIALS_PSW | docker login -u $script.DOCKERHUB_CREDENTIALS_USR --password-stdin"
+    }
+
+    def pushDockerImage(String tag){
+
         script.sh "docker push medch1/$tag "
     }
 
